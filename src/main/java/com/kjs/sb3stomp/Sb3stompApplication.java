847e4c7d934c07c68a845fbd5c1102be2a01ef59
whatnameshouldido <pobyjun@naver.com>
2024-11-04 19:35:51 +0900

    add sb3stomp
    add sb3stomp

---
+++ src/main/java/com/kjs/sb3stomp/Sb3stompApplication.java
@@ -0,0 +1,13 @@
+package com.kjs.sb3stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3stompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb3stompApplication.class, args);
+    }
+
+}
